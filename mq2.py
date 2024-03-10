556c796ea501c65459af5658fbe49227b1c48558
Yu Sheng <37260088+NotYuSheng@users.noreply.github.com>
2024-03-10 23:34:16 +0800

    Update mq2.py

--- mq2.py
+++ mq2.py
@@ -176,7 +176,7 @@ class MQ2():
     #          value.
     ############################################################################ 
     def MQGetPercentage(self, rs_ro_ratio, pcurve):
-        print(f"math.log(rs_ro_ratio): {math.log(rs_ro_ratio)}")
+        print(f"rs_ro_ratio: {rs_ro_ratio}")
         print(f"pcurve[0]: {pcurve[0]}")
         print(f"pcurve[1]: {pcurve[1]}")
         print(f"pcurve[2]: {pcurve[2]}")
