e6a4ce59b26b1856dba6ec6e5c1c4c340b1676bd
Yu Sheng <37260088+NotYuSheng@users.noreply.github.com>
2024-03-15 15:53:05 +0800

    Update mq2-mq135-test.py

--- mq2-mq135-test.py
+++ mq2-mq135-test.py
@@ -2,39 +2,39 @@ import sys, time, os
 from lib.mq2 import *
 from lib.mq135 import *
 
-try:
-    mq2 = MQ2();
-    mq135 = MQ135();
-    print("Press CTRL+C to abort.")
-    
-    while True:
-        percMQ2 = mq2.MQPercentage()
-        percMQ135 = mq135.MQPercentage()
-    
-        lpgPPM = round(percMQ2["LPG"], 2)
-        coPPM = round(percMQ2["CO"], 2)
-        smokePPM = round(percMQ2["SMOKE"], 2)
-        propanePPM = round(percMQ2["PROPANE"], 2)
-        h2PPM = round(percMQ2["H2"], 2) # Hydrogen
-        alcoholPPM = round(percMQ2["ALCOHOL"], 2)
-        ch4PPM = round(percMQ2["CH4"], 2) # Methane
-    
-        acetonPPM = round(percMQ135["ACETON"], 2)
-        toluenoPPM = round(percMQ135["TOLUENO"], 2)
-        alcoholPPM = round(percMQ135["ALCOHOL"], 2)
-        co2PPM = round(percMQ135["CO2"], 2)
-        nh4PPM = round(percMQ135["NH4"], 2)
-        coPPM = round(percMQ135["CO"], 2)
-        
-        os.system("clear")
-        print("----------MQ2----------")
-        print("LPG: %g ppm, CO: %g ppm, Smoke %g ppm, Propane %g ppm, H2 %g ppm, Alcohol: %g ppm, CH4: %g ppm" % (lpgPPM, coPPM, smokePPM, propanePPM, h2PPM, alcoholPPM, ch4PPM))
-        print("---------MQ135---------")
-        print("ACETON: %g ppm, TOLUENO: %g ppm, ALCOHOL: %g ppm, CO2: %g ppm, NH4: %g ppm, CO: %g ppm" % (acetonPPM, toluenoPPM, alcoholPPM, co2PPM, nh4PPM, coPPM))
-        
-        time.sleep(0.1)
+#try:
+mq2 = MQ2();
+mq135 = MQ135();
+print("Press CTRL+C to abort.")
 
+while True:
+    percMQ2 = mq2.MQPercentage()
+    percMQ135 = mq135.MQPercentage()
+
+    lpgPPM = round(percMQ2["LPG"], 2)
+    coPPM = round(percMQ2["CO"], 2)
+    smokePPM = round(percMQ2["SMOKE"], 2)
+    propanePPM = round(percMQ2["PROPANE"], 2)
+    h2PPM = round(percMQ2["H2"], 2) # Hydrogen
+    alcoholPPM = round(percMQ2["ALCOHOL"], 2)
+    ch4PPM = round(percMQ2["CH4"], 2) # Methane
+
+    acetonPPM = round(percMQ135["ACETON"], 2)
+    toluenoPPM = round(percMQ135["TOLUENO"], 2)
+    alcoholPPM = round(percMQ135["ALCOHOL"], 2)
+    co2PPM = round(percMQ135["CO2"], 2)
+    nh4PPM = round(percMQ135["NH4"], 2)
+    coPPM = round(percMQ135["CO"], 2)
+    
+    os.system("clear")
+    print("----------MQ2----------")
+    print("LPG: %g ppm, CO: %g ppm, Smoke %g ppm, Propane %g ppm, H2 %g ppm, Alcohol: %g ppm, CH4: %g ppm" % (lpgPPM, coPPM, smokePPM, propanePPM, h2PPM, alcoholPPM, ch4PPM))
+    print("---------MQ135---------")
+    print("ACETON: %g ppm, TOLUENO: %g ppm, ALCOHOL: %g ppm, CO2: %g ppm, NH4: %g ppm, CO: %g ppm" % (acetonPPM, toluenoPPM, alcoholPPM, co2PPM, nh4PPM, coPPM))
+    
+    time.sleep(0.1)
+"""
 except Exception as e:
     print(e)
     print("\nAbort by user")
-
+"""
