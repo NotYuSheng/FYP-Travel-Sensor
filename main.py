7fa2df78e9a77409ffc478c72fba78c310bcc116
Yu Sheng <37260088+NotYuSheng@users.noreply.github.com>
2024-03-15 19:05:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -390,13 +390,13 @@ if __name__ == "__main__":
     # adc Declaration
     adc = MCP3008()
     
-    
+    """
     while True:
         os.system("clear")
         print(f"Smoke: {adc.read(MQ2_MCP3008_PIN)}")
         print(f"Air Quality: {adc.read(MQ135_MCP3008_PIN)}")
         time.sleep(1)
-    
+    """
     
     print("Initialization complete!")
     main()
